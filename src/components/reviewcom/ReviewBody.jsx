e87fa91314735f94b26413e7ba9ca3fc666f54c9
jolennon77 <jolennon77@gmail.com>
2024-05-24 10:46:05 +0900

    0524프론트배포 2트

--- src/components/reviewcom/ReviewBody.jsx
+++ src/components/reviewcom/ReviewBody.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import ImageDisplay from './ImageDisplay';
 import ReviewCard from './ReviewCard';
 
 const ReviewBody =() => {
