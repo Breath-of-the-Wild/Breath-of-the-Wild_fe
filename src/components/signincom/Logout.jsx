3aa98f316b05cc17021400508a7e7791f87edd29
YangShin <g56261822@gmail.com>
2024-05-26 17:36:43 +0900

    28

--- src/components/signincom/Logout.jsx
+++ src/components/signincom/Logout.jsx
@@ -15,6 +15,12 @@ function Logout() {
       localStorage.removeItem("username");
       Cookies.remove("access_token"); // 쿠키 삭제
       Cookies.remove("refresh_token"); // 쿠키 삭제
+      localStorage.removeItem('accessToken');
+      localStorage.removeItem('accessTokenExpiry');
+  
+      // 쿠키에서 토큰을 삭제합니다.
+      Cookies.remove('accessToken');
+      Cookies.remove('refreshToken');
 
       setAuth(null);
       navigate("/");
