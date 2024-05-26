704ab1b6b42f787d70d569f0adc6055404ff80c6
YangShin <g56261822@gmail.com>
2024-05-26 18:30:42 +0900

    43

--- src/components/signincom/SignInExam.jsx
+++ src/components/signincom/SignInExam.jsx
@@ -50,8 +50,8 @@ const SignInexam = () => {
 
         alert(resp.data.username + "님, 성공적으로 로그인 되었습니다 🔐");
 
-        const accessToken = resp.data.access_token;
-        const refreshToken = resp.data.refresh_token;
+        const accessToken = resp.data.token;
+        const refreshToken = resp.data.refreshToken;
 
         // Set access token to expire in 1 hour
         Cookies.set('access_token', accessToken, { expires: 1 / 24, secure: true, sameSite: 'Strict', path: '/' });
@@ -159,15 +159,24 @@ const SignInexam = () => {
                   </Typography>
                 </div>
 
-                {/* 버튼 */}
-                <div className="mb-1 flex flex-col gap-4 relative">
+                       {/* 버튼 */}
+                       <div className="mb-1 flex flex-col gap-4 relative">
                   <Button size="lg" className="mt-0" onClick={login} fullWidth>
                     로그인
                   </Button>
                 </div>
-
+                <Button size="lg" color="white" className="flex items-center gap-2 justify-center shadow-md" fullWidth onClick={onKakaoLogin}>
+    <img src='/img/icon/kakaologo1.png' style={{ width: '18px', height: '18px' }} />
+    <span>카카오 계정으로 로그인</span>
+  </Button>
                 <div className="mb-1 flex flex-col gap-4 relative">
-                  <Button size="lg" color="white" className="flex items-center gap-2 justify-center shadow-md" fullWidth onClick={onGoogleLogin}>
+
+<div className="mb-1 flex flex-col gap-4">
+  <Button size="lg" color="white" className="flex items-center gap-2 justify-center shadow-md" fullWidth onClick={onNaverLogin}>
+    <img src='/img/icon/naverlogo.png' style={{ width: '18px', height: '18px' }} />
+    <span>네이버 계정으로 로그인</span>
+  </Button>
+  <Button size="lg" color="white" className="flex items-center gap-2 justify-center shadow-md" fullWidth onClick={onGoogleLogin}>
                     <svg width="17" height="16" viewBox="0 0 17 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                       <g clipPath="url(#clip0_1156_824)">
                         <path d="M16.3442 8.18429C16.3442 7.64047 16.3001 7.09371 16.206 6.55872H8.66016V9.63937H12.9813C12.802 10.6329 12.2258 11.5119 11.3822 12.0704V14.0693H13.9602C15.4741 12.6759 16.3442 10.6182 16.3442 8.18429Z" fill="#4285F4" />
@@ -183,6 +192,9 @@ const SignInexam = () => {
                     </svg>
                     <span>구글 계정으로 로그인</span>
                   </Button>
+</div>
+
+
                 </div>
                 <div className="mb-1 flex flex-col gap-4 relative">
                   <Typography variant="paragraph" className="text-center text-blue-gray-500 font-medium mt-4">
