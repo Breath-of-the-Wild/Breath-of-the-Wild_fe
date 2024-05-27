cda1fca672f07c33789a4d944f44233317703522
YangShin <g56261822@gmail.com>
2024-05-27 09:14:40 +0900

    ....

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,5 +1,4 @@
 
-import Login from '@/components/signincom/Login';
 import './home.css'
 import Top from '@/components/homecom/Top';
 
@@ -7,7 +6,6 @@ export function Home() {
   return (
     <>
           <Top />
-          <Login />
     </>
   );
 }
